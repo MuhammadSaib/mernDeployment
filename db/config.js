00d52e5f1f48b629a98a663044b60d5571ef7a63
MuhammadSaib12 <l217708@lhr.nu.edu.pk>
2024-08-07 16:24:23 +0500

    local path change

--- db/config.js
+++ db/config.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
-const BASE_URL = process.env.BASE_URL;
 const mongoose = require('mongoose');
-mongoose.connect(`${BASE_URL}/SEProject`)
+const BASE_URL = process.env.BASE_URL;
+
+mongoose.connect(BASE_URL)
   .then(() => {
     console.log('Database connection successful');
   })
   .catch(err => {
     console.error('Database connection error:', err);
-});
+  });
